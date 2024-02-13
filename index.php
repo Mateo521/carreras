9f5ee233411657c737a60686b70c08d7891d6fa6
Mateo521 <mateovillalva42@gmail.com>
2024-02-12 22:23:38 -0300

    fixs

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if ($conn->connect_error) {
             $anios = $anios->item(0)->textContent;
         } 
 
-       
+       /*no funca*/
     ?>
         <form method="post" action="" class="p-2">
             <label for="url">Selecciona una URL:</label>
